2d299a466381e1d39651193bf906ea84cf5aecaf
Adam Elders <Ramor.1990@gmail.com>
2018-04-20 12:19:26 -0400

    First commit of Haunted Zombie Rush project. Already added the background and set camera. Added the moving floor, and started adding objects to avoid.

---
+++ Unity Course Examples/Haunted Zombie Rush/Assets/Scripts/Platform.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour {
+
+    [SerializeField] float objectSpeed = 8;
+    private float resetPosition = 30f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.left * (objectSpeed * Time.deltaTime));
+
+        if (transform.localPosition.x > resetPosition) {
+            Vector3 newPos = new Vector3(-82, transform.position.y, transform.position.z);
+            transform.position = newPos;
+        }
+	}
+}
