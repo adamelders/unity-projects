bb5e6755fbb24e90c52af94dcddaa38730ef935f
Adam Elders <Ramor.1990@gmail.com>
2018-04-24 20:55:41 -0400

    1. Added enemy sprites and added animations.
    2. Added a GameManager script to spawn enemies.
    3. Added waypoints and implemented pathing for enemies.

---
+++ Unity Course Examples/Tower Defense/Assets/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+    public GameObject spawnPoint;
+    public GameObject[] enemies;
+    public int maxEnemiesOnScreen;
+    public int totalEnemies;
+    public int enemiesPerSpawn;
+    public float spawnDelay;
+
+    private int enemiesOnScreen = 0;
+
+    private void Awake() {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(Spawn());
+	}
+
+    public void RemoveEnemyFromScreen() {
+        if (enemiesOnScreen > 0)
+            enemiesOnScreen--;
+    }
+
+    IEnumerator Spawn() {
+        if (enemiesPerSpawn > 0 && enemiesOnScreen < totalEnemies) {
+            for (int i = 0; i < enemiesPerSpawn; i++) {
+                if (enemiesOnScreen < maxEnemiesOnScreen) {
+                    int randomNumber = Random.Range(0, 3);
+                    GameObject newEnemy = Instantiate(enemies[randomNumber]) as GameObject;
+                    newEnemy.transform.position = spawnPoint.transform.position;
+                    enemiesOnScreen++;
+                }
+            }
+
+            yield return new WaitForSeconds(spawnDelay);
+            StartCoroutine(Spawn());
+        }
+    }
+}
